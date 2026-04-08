c83be106fc1df9afd40e732f5a5bd3c1abadf562
ananthikaa <ananthika.anup@gmail.com>
2026-04-08 14:21:51 -0400

    Task 3: formatted output with dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -15,4 +16,25 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(URL, headers=Headers, json=Input_json)
     
-    return response.text
+    # convert response to dictionary
+    response_dict = json.loads(response.text)
+    
+    emotions = response_dict['emotionPredictions'][0]['emotion']
+    
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+    
+    # find dominant emotion
+    dominant_emotion = max(emotions, key=emotions.get)
+    
+    return {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
+    }
